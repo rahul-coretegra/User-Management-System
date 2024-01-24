3c5b9be18376f3f9b7878a336ef68c05bcf3229c
rahul-coretegra <rahul@coretegra.com>
2024-01-24 15:08:02 +0530

    Packages And Db Setup

--- User Management System/Program.cs
+++ User Management System/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using User_Management_System.DbModule;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
